12663dbe7c408b0e70e01e75f93246e2d7af98d4
merveille228 <merveilleamevigbe2@gmail.com>
2025-09-12 18:03:59 +0000

    voici une version avec les citations de succes

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
-let content = [
-    "coucou",
-    "it's me",
-    "aude est un fou",
-    "gildas un mito"
-];
+
 
 let btn = document.querySelector("button");
 let texte = document.getElementById("content");
 
-btn.addEventListener("click", function() {
-    let index = Math.floor(Math.random() * content.length);
-    texte.textContent = content[index];
-});
